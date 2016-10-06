9afa38325ecc9d18872cd2a078d37e91bcd2b1a8
Daniel Patrick Fairbanks <fairbanksdp@dcsdk12.org>
2016-10-06 14:20:21 -0600

    Implement guest

---
+++ pot_luck.rb
@@ -0,0 +1,60 @@
+require './scan'
+people = open('people.csv')
+$people = people.each_line.map do|txt|
+  #name,email,guests,food =  ("#{txt}").split(',')
+  "#{txt}"
+end 
+people.close
+foods = open('foods.csv')
+$foods = foods.each_line.map {|txt|txt}
+foods.close
+
+def guest()
+  guest_count = 0
+  $people.each do|txt|
+    n,n,guests =  ("#{txt}").split(',')
+    guest_count += guests.to_i + 1
+  end
+  guest_count
+end
+
+print guest
+print "\n"
+
+def rsvp()
+  info = scan.chomp
+  if invalid_request?(info)
+    print "Error. Try again. \n"
+    rsvp
+  else 
+    people = open('people.csv','a')
+    people.write info
+    people.close
+    
+    n,n,n,food = info.split(',')
+    $foods.delete(food)
+    $foods.push(nil)
+    foods = open('foods.csv','w')
+    counter = 0
+    foods.each_line do|line|
+      foods.replace_puts($foods[counter])
+      counter += 1
+    end
+
+    foods.close
+        
+  end
+end
+
+def invalid_request?(info)
+  name, email, guests, food = info.split(',')
+  if guests.to_i < 1 || guests.to_i > 10
+    return false
+  end
+  if $foods.include?(food)
+    return true
+  end
+  false
+end
+
+rsvp
